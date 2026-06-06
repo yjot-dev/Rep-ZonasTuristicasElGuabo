9684e62e5267bf3955996dfecc9bedda83acf505
yjot2 <2010guabo@gmail.com>
2026-06-06 14:16:48 -0500

    Subida de codigo de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yjotdev.zonasturisticaselguabo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
         testInstrumentationRunner = "com.yjotdev.zonasturisticaselguabo.CustomTestRunner"
     }
     signingConfigs {
